c678c6023744ed399568a456ff06e6788f0a5ac5
Jeroen Visser <jeroenvisser101@gmail.com>
2021-08-02 13:32:16 +0200

    Ensure dialyzer considers Flow and GenStage as deps

--- mix.exs
+++ mix.exs
@@ -55,6 +55,7 @@ defmodule BarcodeGenerator.MixProject do
 
   defp dialyzer do
     [
+      plt_add_apps: [:flow, :gen_stage],
       plt_file:
         {:no_warn, ".dialyzer/elixir-#{System.version()}-erlang-otp-#{System.otp_release()}.plt"}
     ]
